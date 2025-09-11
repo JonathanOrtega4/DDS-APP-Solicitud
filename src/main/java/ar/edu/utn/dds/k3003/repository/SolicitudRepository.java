11038185f457ff341b58a8cce9180d2d4d6625a8
JonathanOrtega4 <joortega@frba.utn.edu.ar>
2025-09-11 16:40:45 -0300

    Update SolicitudRepository.java
    cambios interface

--- src/main/java/ar/edu/utn/dds/k3003/repository/SolicitudRepository.java
+++ src/main/java/ar/edu/utn/dds/k3003/repository/SolicitudRepository.java
@@ -1,22 +1,14 @@
 package ar.edu.utn.dds.k3003.repository;
 
 import ar.edu.utn.dds.k3003.model.Solicitud;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface SolicitudRepository extends JpaRepository<Solicitud, String> {
-
+public interface SolicitudRepository {
     Optional<Solicitud> findById(String id);
     List<Solicitud> findByHechoId(String id);
-
     Solicitud save(Solicitud solicitud);
-
-    //Solicitud save(Solicitud solicitud);
     void delete(String id);
-
     List<Solicitud> findAll();
 }
